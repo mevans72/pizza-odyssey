21a14706f78b5859c2af1f4c4a7176df7b84d7f3
mevans72 <michaleevans72@gmail.com>
2016-02-17 16:53:23 -0800

    Updating more stuff in app.js to incude the rendering function. Still a work in progress

--- app.js
+++ app.js
@@ -1,69 +1,90 @@
 'use strict';
-
-//Build a constructor function to be leveraged for all of the stores
+//Store your results:
+//Constructor Objects
 function PizzaShop (locationName, storeData) {
   this.locationName = locationName;
   this.storeData = storeData;
-  this.reportData = function(min, max) {
-    for(var i = 0; i < this.reportData.length; i++) {
-      var numPizzas = this.generateRandom(this.dataSet[i][1], this.dataSet[i][2]);
-      var numDeliveries = this.generateRandom(this.dataSet[i][3], this.dataSet[i][4]);
-    }
-  };
-  this.createTable = function createTable(inputArray) {
-    var row = document.createElement('tr');
-    for(var i = 0; i < inputArray.length; i++) {
-      var column = document.createElement('td');
-      column.textContent = inputArray[i];
-      row.appendChild(column);
-    }
-    console.table(inputArray);
-    return row;
-  }
+  this.pizzasPerDay = [];
+  this.deliveriesPerDay = [];
+  this.pizzasPerHour = [];
+  this.deliveriesPerHour = [];
 
-}
+};
+//Display Your Results
+//Declare Store Functions and Save Results to New Store Variables
+var ballardData = [[0, 4, 0, 4], [0, 4, 0, 7], [2, 15, 1, 4], [15, 35, 3, 8], [ 12, 31, 5, 12], [5, 20, 6, 11]];
+var ballard = new PizzaShop('Ballard', ballardData);
+var firstHillData = [[0, 4, 0, 4], [0, 4, 0, 7], [2, 15, 1, 4], [15, 35, 3, 8], [ 12, 31, 5, 12], [5, 20, 6, 11]];
+var firstHill = new PizzaShop('First Hill', firstHillData);
 
-PizzaShop.prototype.generateRandom = function() {
+PizzaShop.prototype.generateRandom = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+PizzaShop.prototype.calcPizzasandDeliveriesPerHr = function() {
+  for(var i = 0; i < this.storeData.length; i++) {
+    for(var j = 0; j < 3; j++) {
+      var singleHour = this.generateRandom(this.storeData[i][0], this.storeData[i][1]);
+      this.pizzasPerHour.push(singleHour);
+      var singleHour = this.generateRandom(this.storeData[i][2], this.storeData[i][3]);
+      this.deliveriesPerHour.push(singleHour);
+    }
+  }
+};
+ballard.calcPizzasandDeliveriesPerHr();
+
+PizzaShop.prototype.render = function() {
+  var shopTable = document.createElement('table');
+  var row = document.createElement('tr');
+  for(var i = 0; i < this.storeData.length; i++) {
+    var column = document.createElement('td');
+    column.textContent = this.storeData[i];
+    row.appendChild(column);
+  }
+  document.body.appendChild(shopTable);
+  console.table(this.storeData);
+  return row;
+};
+
+PizzaShop.prototype.render = function() {
+
+  this.calcPizzasandDeliveriesPerHr();
+  ///to make the 1st header row
+  var shopTable = document.createElement('table');
+  var trHeader1 = document.createElement('tr');
+
+  var tdEl = document.createElement('td');
+  tdEl.textContent = 'MinPizza\'s';
+  trHeader1.appendChild(tdEl);
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = 'MaxPizza\'s';
+  trHeader1.appendChild(tdEl);
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = 'MinDeliveries\'s';
+  trHeader1.appendChild(tdEl);
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = 'MaxDeliveries\'s';
+  trHeader1.appendChild(tdEl);
+
+  shopTable.appendChild(trHeader1);
+
+  for (var i = 0; i < this.storeData.length; i++) {
+
+    var trEl = document.createElement('tr');
+    tdEl = document.createElement('td');
+    tdEl.textContent = (this.storeData[i]);
+    trEl.appendChild(tdEl);
+
+    // tdEl = document.createElement('td');
+    // tdEl.textContent = this.pizzasEachHour[i];
+    // trEl.appendChild(tdEl);
+
+    shopTable.appendChild(trEl);
+  }
+  document.body.appendChild(shopTable);
 }
 
-// PizzaShop.prototype.reportData = function(min, max) {
-//   for(var i = 0; i < this.reportData.length; i++) {
-//     var numPizzas = this.generateRandom(dataSet[i][1], this.dataSet[i][2]);
-//     var numDeliveries = this.generateRandom(dataSet[i][3], this.dataSet[i][4]);
-//   };
-// }
-
-//PizzaShop.prototype.calcPizzasPerHr = function() {
-//
-// }
-
-// PizzaShop.prototype.makeTableRow = function createTableRow(inputArray) {
-//   var row = document.createElement('tr');
-//   for(var i = 0; i < inputArray.length; i++) {
-//     var column = document.createElement('td');
-//     column.textContent = inputArray[i];
-//     row.appendChild(column);
-//   }
-//   console.table(inputArray);
-//   return row;
-// }
-
-
-var ballardData = [['Time', 'Min Pizza/hr', 'Max Pizza/hr', 'Min Delivery/hr', 'Max Delivery/hr'],['8am-11am', 0, 4, 0, 4], ['11am-2pm', 0, 4, 0, 7], ['2pm-5pm', 2, 15, 1, 4], ['5pm-8pm', 15, 35, 3, 8], ['8pm-11pm', 12, 31, 5, 12], ['11pm-2am', 5, 20, 6, 11]];
-var ballard = new PizzaShop('ballard', ballardData);
-
-var firstHillData = [['Time', 'Min Pizza/hr', 'Max Pizza/hr', 'Min Delivery/hr', 'Max Delivery/hr'], ['8am-11am', 1, 3, 1, 7], ['11am-2pm', 5, 9, 2, 8], ['2pm-5pm', 2, 13, 1, 6], ['5pm-8pm', 18, 32, 3, 9], ['8pm-11pm', 1, 3, 5, 12], ['11pm-2am', 8, 20, 6, 16]];
-// var firstHill = new PizzaShop('firstHill', firstHillData);
-
-var tidData = [['Time', 'Min Pizza/hr', 'Max Pizza/hr', 'Min Delivery/hr', 'Max Delivery/hr'], ['8am-11am', 0, 4, 0, 4], ['11am-2pm', 0, 7, 0, 4], ['2pm-5pm', 2, 15, 1, 4], ['5pm-8pm', 10, 26, 4, 6], ['8pm-11pm', 8, 22, 7, 15], ['11pm-2am', 0, 2, 2, 8]];
-// var tid = new PizzaShop('tid', tidData);
-
-var sluData = [['Time', 'Min Pizza/hr', 'Max Pizza/hr', 'Min Delivery/hr', 'Max Delivery/hr'], ['8am-11am', 0, 4, 0, 4], ['11am-2pm', 0, 7, 0, 4], ['2pm-5pm', 5, 15, 0, 4], ['5pm-8pm', 25, 39, 13, 18], ['8pm-11pm', 22, 36, 5, 22], ['11pm-2am', 5, 21, 16, 31]];
-// var slu = new PizzaShop('slu', sluData);
-
-var georgetownData = [['Time', 'Min Pizza/hr', 'Max Pizza/hr', 'Min Delivery/hr', 'Max Delivery/hr'], ['8am-11am', 2, 7, 3, 5], ['11am-2pm', 3, 8, 3, 9], ['2pm-5pm', 1, 5, 1, 4], ['5pm-8pm', 5, 13, 2, 4], ['8pm-11pm', 22, 41, 15, 42], ['11pm-2am', 15, 20, 6, 21]];
-// var georgetown = new PizzaShop('georgetown', georgetownData);
-
-var ravennaData = [['Time', 'Min Pizza/hr', 'Max Pizza/hr', 'Min Delivery/hr', 'Max Delivery/hr'], ['8am-11am', 0, 4, 0, 4], ['11am-2pm', 0, 7, 0, 4], ['2pm-5pm', 2, 15, 1, 4], ['5pm-8pm', 6, 9, 5, 18], ['8pm-11pm', 2, 8, 2, 5], ['11pm-2am', 2, 4, 3, 11]];
-// var ravenna = new PizzaShop('ravenna', ravennaData);
+ballard.render();
