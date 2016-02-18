ad64680a4e1394ded17836a24ec8f68061bf45ca
mevans72 <michaleevans72@gmail.com>
2016-02-18 07:31:53 -0800

    Minor changes. Still a work in progress on the tables...

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ function PizzaShop (locationName, storeData) {
   this.deliveriesPerDay = [];
   this.pizzasPerHour = [];
   this.deliveriesPerHour = [];
+  this.storeTableData = [];
 
 };
 //Display Your Results
 //Declare Store Functions and Save Results to New Store Variables
 var ballardData = [[0, 4, 0, 4], [0, 4, 0, 7], [2, 15, 1, 4], [15, 35, 3, 8], [ 12, 31, 5, 12], [5, 20, 6, 11]];
 var ballard = new PizzaShop('Ballard', ballardData);
-var firstHillData = [[0, 4, 0, 4], [0, 4, 0, 7], [2, 15, 1, 4], [15, 35, 3, 8], [ 12, 31, 5, 12], [5, 20, 6, 11]];
+var firstHillData = [[2, 4, 2, 4], [2, 4, 2, 7], [2, 3, 1, 3], [15, 35, 3, 8], [ 12, 31, 5, 12], [5, 20, 6, 11]];
 var firstHill = new PizzaShop('First Hill', firstHillData);
 
 PizzaShop.prototype.generateRandom = function(min, max) {
@@ -34,6 +35,7 @@ PizzaShop.prototype.calcPizzasandDeliveriesPerHr = function() {
 ballard.calcPizzasandDeliveriesPerHr();
 
 PizzaShop.prototype.render = function() {
+  var tablearea = document.getElementById(this.locationName);
   var shopTable = document.createElement('table');
   var row = document.createElement('tr');
   for(var i = 0; i < this.storeData.length; i++) {
@@ -41,50 +43,100 @@ PizzaShop.prototype.render = function() {
     column.textContent = this.storeData[i];
     row.appendChild(column);
   }
+  this.storeTableData.push(shopTable);
   document.body.appendChild(shopTable);
+  console.log(this.locationName);
   console.table(this.storeData);
-  return row;
 };
 
+// PizzaShop.prototype.render = function() {
+//
+//   this.calcPizzasandDeliveriesPerHr();
+//   ///to make the 1st header row
+//   var shopTable = document.createElement('table');
+//   var trHeader1 = document.createElement('tr');
+//   var trHeader2 = document.createElement('tr');
+//
+//
+//   var tdEl = document.createElement('td');
+//   tdEl.textContent = 'MinPizza\'s';
+//   trHeader1.appendChild(tdEl);
+//
+//   tdEl = document.createElement('td');
+//   tdEl.textContent = 'MaxPizza\'s';
+//   trHeader1.appendChild(tdEl);
+//
+//   tdEl = document.createElement('td');
+//   tdEl.textContent = 'MinDeliveries\'s';
+//   trHeader1.appendChild(tdEl);
+//
+//   tdEl = document.createElement('td');
+//   tdEl.textContent = 'MaxDeliveries\'s';
+//   trHeader1.appendChild(tdEl);
+//
+//   shopTable.appendChild(trHeader1);
+//
+//   for (var i = 0; i < this.storeData.length; i++) {
+//
+//     var trEl = document.createElement('tr');
+//     tdEl = document.createElement('td');
+//     tdEl.textContent = (this.storeData[i][0]);
+//     trEl.appendChild(tdEl);
+//
+//     shopTable.appendChild(trEl);
+//   }
+//   for (var i = 0; i < this.storeData.length; i++) {
+//
+//     var trEl = document.createElement('tr');
+//     tdEl = document.createElement('td');
+//     tdEl.textContent = (this.storeData[i][0]);
+//     trEl.appendChild(tdEl);
+//
+//     shopTable.appendChild(trEl);
+//   }
+//   document.body.appendChild(shopTable);
+// }
+
+// function createTable(tableData) {
+//   var table = document.createElement('table');
+//   var tableBody = document.createElement('tbody');
+//
+//   tableData.forEach(function(rowData) {
+//     var row = document.createElement('tr');
+//
+//     rowData.forEach(function(cellData) {
+//       var cell = document.createElement('td');
+//       cell.appendChild(document.createTextNode(cellData));
+//       row.appendChild(cell);
+//     });
+//
+//     tableBody.appendChild(row);
+//   });
+//
+//   table.appendChild(tableBody);
+//   document.body.appendChild(table);
+// }
 PizzaShop.prototype.render = function() {
+  var table = document.createElement('table');
+  var tableBody = document.createElement('tbody');
 
-  this.calcPizzasandDeliveriesPerHr();
-  ///to make the 1st header row
-  var shopTable = document.createElement('table');
-  var trHeader1 = document.createElement('tr');
+  this.storeData.forEach(function(rowData) {
+    var row = document.createElement('tr');
 
-  var tdEl = document.createElement('td');
-  tdEl.textContent = 'MinPizza\'s';
-  trHeader1.appendChild(tdEl);
+    rowData.forEach(function(cellData) {
+      var cell = document.createElement('td');
+      cell.appendChild(document.createTextNode(cellData));
+      row.appendChild(cell);
+    });
 
-  tdEl = document.createElement('td');
-  tdEl.textContent = 'MaxPizza\'s';
-  trHeader1.appendChild(tdEl);
+    tableBody.appendChild(row);
+  });
 
-  tdEl = document.createElement('td');
-  tdEl.textContent = 'MinDeliveries\'s';
-  trHeader1.appendChild(tdEl);
+  table.appendChild(tableBody);
+  document.body.appendChild(table);
+};
 
-  tdEl = document.createElement('td');
-  tdEl.textContent = 'MaxDeliveries\'s';
-  trHeader1.appendChild(tdEl);
 
-  shopTable.appendChild(trHeader1);
-
-  for (var i = 0; i < this.storeData.length; i++) {
-
-    var trEl = document.createElement('tr');
-    tdEl = document.createElement('td');
-    tdEl.textContent = (this.storeData[i]);
-    trEl.appendChild(tdEl);
-
-    // tdEl = document.createElement('td');
-    // tdEl.textContent = this.pizzasEachHour[i];
-    // trEl.appendChild(tdEl);
-
-    shopTable.appendChild(trEl);
-  }
-  document.body.appendChild(shopTable);
-}
 
 ballard.render();
+firstHill.render();
